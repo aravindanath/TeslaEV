1ce5b3bc8aada28faff12e5a288cb14f9dececb1
aravindanath <aravindanath86@gmail.com>
2019-12-17 08:21:11 +0530

    chrome options

--- PycharmProjects/PythonSeleniumAutoAugSep/browserLaunch/launchFireFox.py
+++ PycharmProjects/PythonSeleniumAutoAugSep/browserLaunch/launchFireFox.py
@@ -1,14 +1,12 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver import Firefox
 
 # /Users/aravindanathdm/Documents/TeslaEV/PycharmProjects/PythonSeleniumAutoAugSep/driver/chromedriver
 # winPath = "..\\driver\\chromedriver.exe"
 # For MAC
 # Relative path
 
-path = "../driver/chromedriver"
-ops = ChromeOptions()
-# ops.add_argument("--headless")
-ops.add_argument("--disable-notifications")
-driver = Chrome(executable_path=path,chrome_options=ops)
-# driver.get("https://www.amazon.in/")
+path = "../driver/geckodriver"
+
+driver = Firefox(executable_path=path)
